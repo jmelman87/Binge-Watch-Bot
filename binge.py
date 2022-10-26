afe1ec7c621fd3426cddd07c6bfbd308fad21962
jmelman87 <34871736+jmelman87@users.noreply.github.com>
2022-10-26 15:46:28 -0400

    Add files via upload

---
+++ binge.py
@@ -0,0 +1,19 @@
+import time
+import pyautogui
+
+
+# THIS SCRIPT RUNS A SIMPLE LOOP THAT WILL MOVE THE MOUSE SLIGHTLY ON THE SCREEN EVERY 1 HOUR
+# WILL PREVENT NETFLIX, HULU, DISNEY+ AND ALL STREAMING SERVICES FROM HAVING A TIMEOUT AFTER INACTIVITY FOR X TIME
+
+# REMEMBER: The upper left corner of the computer screen is (0,0)
+
+while True:
+    pyautogui.moveTo(500, 350) # moves mouse to somewhere in the middle of the screen (ok if not exactly in center - bot will work)
+
+    time.sleep(3600) #sleep for 1 hour
+
+    pyautogui.move(200, 100) #moves mouse right 200px, down 100px
+    time.sleep(0.5)
+    pyautogui.move(-200, -100) #moves mouse backwards - left 200px, up 100px
+    
+
